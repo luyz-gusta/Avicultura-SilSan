4c6f866a739b6c3c4cdb8b73c5c93c63453e45c6
Renan <renan.yuuji@itpower.co>
2023-09-19 10:38:23 -0300

    Add

---
+++ js/cadastroLojista/api.js
@@ -0,0 +1,33 @@
+'use strict'
+
+export const createLojistaUsuario = async (lojista) => {
+    const url = `https://avicultura-silsa-api.cyclic.app/v1/avicultura-silsan/lojista-usuario`
+
+    const options = {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json'
+        },
+        body: JSON.stringify(lojista)
+    };
+
+    fetch(url, options)
+        .then(response => {
+            if (response.ok) {
+                location.reload();
+            } else {
+                alert('Erro ao criar a o usuario do lijista confira os valores')
+                console.log('Erro ao criar lojista.');
+                console.log(response.status);
+                console.log(response.json);
+                console.log(response.text);
+                console.log(response.body);
+                console.log(response.ok);
+            }
+        })
+        .catch(error => {
+            console.log('Ocorreu um erro na requisição:', error);
+        });
+}
+
+
--- js/cadastroLojista/main.js
+++ js/cadastroLojista/main.js
@@ -1,8 +1,49 @@
 'use strict'
 
-const cadastrarLojista = () => {
+import { createLojistaUsuario } from "./api.js"
+
+// const cadastrarLojista = async (event) => {
+
+//     event.preventDefault();
+
+//     const nome = document.getElementById('inputNome').value
+//     const telefone = document.getElementById('inputTelefone').value
+//     const email = document.getElementById('inputEmail').value
+//     const senha = document.getElementById('inputSenha').value
+//     const confirmacaoSenha = document.getElementById('inputConfirmeSenha').value
+
+//     if (
+//         nome == null || nome == '' ||
+//         telefone == null || telefone == '' ||
+//         dataNascimento == null || dataNascimento == '' ||
+//         email == null || email == '' ||
+//         senha == null || senha == '' ||
+//         confirmacaoSenha == null || confirmacaoSenha == ''
+//     ) {
+//         alert('Require Fields')
+//     } else if (senha != confirmacaoSenha) {
+//         alert('confirmacao de senha diferente da senha')
+//     } else if (telefone.lenth > 12) {
+//         alert('telefone digitado incorretamente')
+//     } else {
+//         let dadosLojistaUsuario = {
+//             email_usuario: email,
+//             senha_usuario: senha,
+//             nome_lojista: nome,
+//             telefone_lojista:telefone
+//         }
+
+//         console.log(dadosLojistaUsuario);
+
+//         //await createLojistaUsuario(dadosLojistaUsuario)
+//     }
+// }
+
+document.getElementById('btn-enviar').addEventListener('click', async (event) => {
+    event.preventDefault();
+
     const nome = document.getElementById('inputNome').value
-    const telefone = document.getElementById('inputTelefone').value
+    const telefone = document.querySelector('#inputTelefone').value
     const email = document.getElementById('inputEmail').value
     const senha = document.getElementById('inputSenha').value
     const confirmacaoSenha = document.getElementById('inputConfirmeSenha').value
@@ -10,7 +51,6 @@ const cadastrarLojista = () => {
     if (
         nome == null || nome == '' ||
         telefone == null || telefone == '' ||
-        dataNascimento == null || dataNascimento == '' ||
         email == null || email == '' ||
         senha == null || senha == '' ||
         confirmacaoSenha == null || confirmacaoSenha == ''
@@ -21,8 +61,15 @@ const cadastrarLojista = () => {
     } else if (telefone.lenth > 12) {
         alert('telefone digitado incorretamente')
     } else {
-        alert('teste')
-    }
-}
+        let dadosLojistaUsuario = {
+            email_usuario: email,
+            senha_usuario: senha,
+            nome_lojista: nome,
+            telefone_lojista:telefone
+        }
 
-document.getElementById('btn-enviar').addEventListener('click', cadastrarLojista)
+        console.log(dadosLojistaUsuario);
+
+        await createLojistaUsuario(dadosLojistaUsuario)
+    }
+})
